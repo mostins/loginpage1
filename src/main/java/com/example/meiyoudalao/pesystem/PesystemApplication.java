5811216ceb0ba5b291300ff168144dcbf6d6074e
jwlcqjtu <1041975538@qq.com>
2019-07-12 11:13:41 +0800

    Initial commit

---
+++ src/main/java/com/example/meiyoudalao/pesystem/PesystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.meiyoudalao.pesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PesystemApplication.class, args);
+    }
+
+}
